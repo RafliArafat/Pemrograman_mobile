0f50d53bb84d89c061751e2352ddc3c1c092966c
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-10-01 19:51:51 +0700

    jobsheet 5

---
+++ jobsheet5/README.md
@@ -0,0 +1,26 @@
+# Laporan Praktikum
+
+## soal 1
+Hasil praktikum 3 langkah 1
+
+![hasil praktikum 3](/jobsheet5/laporan/1.png)
+
+Hasil praktikum 3 langkah 2
+
+![hasil praktikum 3](/jobsheet5/laporan/2.png)
+
+Hasil praktikum 4 langkah 3
+
+![hasil praktikum 3](/jobsheet5/laporan/3.png)
+
+Hasil praktikum 4 langkah 4
+
+![hasil praktikum 3](/jobsheet5/laporan/4.png)
+
+Hasil praktikum 4 langkah 5
+
+![hasil praktikum 3](/jobsheet5/laporan/5.png)
+
+Hasil praktikum 4 langkah 6
+
+![hasil praktikum 3](/jobsheet5/laporan/6.png)
---
+++ jobsheet5/lib/basic_widgets/fab_widget.dart
@@ -0,0 +1,21 @@
+// File: basic_widgets/fab_widget.dart
+import 'package:flutter/material.dart';
+
+class FabWidget extends StatelessWidget {
+  const FabWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          child: const Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
+        ),
+      ),
+    );
+  }
+}
